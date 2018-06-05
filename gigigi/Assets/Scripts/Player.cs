fa35097ffbf5f22c519dc757937cd04a30410915
jyllsarta <illstarred.dive@gmail.com>
2018-06-06 00:32:40 +0900

    デスエリアに入ると死ぬ

---
+++ gigigi/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private ParticleSystem deathEffectPrefab;
+
+    void Move()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector3(inputX, 0, inputZ) * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //死
+        Instantiate(deathEffectPrefab, transform);
+    }
+
+    void Update()
+    {
+        Move();
+    }
+}
